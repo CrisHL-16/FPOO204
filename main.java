6a408248bf40215d136f536abf61e8d77274e4a5
CrisHL-16 <123047961@upq.edu.mx>
2025-01-22 14:43:48 -0600

    Estructura de un archivo Java y manejo de cadenas

---
+++ main.java
@@ -0,0 +1,25 @@
+//importaciones
+import java.util.*;
+
+
+//Clase principal
+class Sintaxis{
+
+    //Metodo main
+    public static void main(String[] args) {
+
+        /*Arriba 
+        las
+        chivas!! */
+        System.out.println("hello, Cristian");
+        String cadenas = "Cristian" + "Hurtado" + "Lucas";
+
+        System.out.println(cadenas);
+        System.out.println(cadenas.length());
+
+        System.out.println(cadenas.substring(2,5));
+        System.out.println(cadenas.substring(2));
+        System.out.println(cadenas.substring(0,5));
+        
+    }
+}
